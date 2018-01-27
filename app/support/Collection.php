8456b954848b664cd9404ad042f71ecafb2d61ef
cemilcelik <cemilcelik84@gmail.com>
2018-01-27 20:09:49 +0300

    Add empty_instantiated_collection_returns_no_items test.
    Add count_is_correct_for_items_passed_in test.

---
+++ app/support/Collection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Support;
+
+class Collection 
+{
+	public $items = [];
+
+	public function __construct(array $items = [])
+	{
+		$this->items = $items;
+	}
+
+	public function get()
+	{
+		return $this->items;
+	}
+
+	public function count()
+	{
+		return count($this->items);
+	}
+}
---
+++ tests/unit/CollectionTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Support\Collection;
+
+class CollectionTest extends \PHPUnit_Framework_TestCase
+{
+	/** @test */
+	public function empty_instantiated_collection_returns_no_items()
+	{
+		$collection = new Collection;
+
+		$this->assertEmpty($collection->get());
+	}
+
+	/** @test */
+	public function count_is_correct_for_items_passed_in()
+	{
+		$collection = new Collection([
+			'one', 'two', 'three'
+		]);
+
+		$this->assertEquals(3, $collection->count());
+	}
+}
