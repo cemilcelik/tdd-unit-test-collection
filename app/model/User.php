b24e89cf09bc6e0b4b397be860a81c872934f3f2
cemilcelik <cemilcelik84@gmail.com>
2018-01-27 15:43:28 +0300

    Code User test and class.

---
+++ app/model/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Model;
+
+class User
+{
+	public $first_name;
+	public $last_name;
+
+	public function __construct(Array $user = [])
+	{
+		if (isset($user['first_name'])) 
+			$this->setFirstName($user['first_name']);
+			
+		if (isset($user['last_name']))
+			$this->setLastName($user['last_name']);
+	}
+
+	public function setFirstName(String $firstName = ''): void
+	{
+		$this->first_name = trim($firstName);
+	}
+
+	public function getFirstName(): string
+	{
+		return $this->first_name;
+	}
+
+	public function setLastName(String $lastName = ''): void
+	{
+		$this->last_name = trim($lastName);
+	}
+
+	public function getLastName(): String
+	{
+		return $this->last_name;
+	}
+
+	public function getFullName(): string
+	{
+		return $this->getFirstName() . ' ' . $this->getLastName();
+	}
+
+	public function getUserProperties(): array
+	{
+		return [
+			'full_name' => $this->getFullName()
+		];
+	}
+}
---
+++ tests/unit/UserTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Model\User;
+
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+	public $user;
+
+	public function setUp()
+	{
+		$this->user = new User;
+	}
+
+	/** @test */
+	public function assert_construct(Type $var = null)
+	{
+		$user = new User([
+			'first_name' => 'John',
+			'last_name' => 'Doe',
+			'email' => 'john@doe.com'
+		]);
+
+		$this->assertEquals($user->getFirstName(), 'John');
+		$this->assertEquals($user->getLastName(), 'Doe');
+	}
+
+	public function testAssertGetFirstName()
+	{
+		$this->user->setFirstName('John');
+
+		$this->assertEquals($this->user->getFirstName(), 'John');
+	}
+
+	/** @test */
+	public function assert_get_last_name()
+	{
+		$this->user->setLastName('Doe');
+
+		$this->assertEquals($this->user->getLastName(), 'Doe');
+	}
+
+	/** @test */
+	public function assert_get_full_name(Type $var = null)
+	{
+		$this->user->setFirstName('John');
+		$this->user->setLastName('Doe');
+
+		$this->assertEquals($this->user->getFullName(), 'John Doe');
+	}
+
+	/** @test */
+	public function assert_trimmed_first_and_last_name()
+	{
+		$this->user->setFirstName('   John   ');
+		$this->user->setLastName('   Doe   ');
+
+		$this->assertEquals($this->user->getFirstName(), 'John');
+		$this->assertEquals($this->user->getLastName(), 'Doe');
+	}
+
+	/** @test */
+	public function assert()
+	{
+		$this->user->setFirstName('John');
+		$this->user->setLastName('Doe');
+		$userProperties = $this->user->getUserProperties();
+
+		$this->assertArrayHasKey('full_name', $userProperties);
+	}
+}
